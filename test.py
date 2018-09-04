12f48e67019bbe8754d39cd7e6faaa44e41a85fb
angelav011301 <42008429+angelav011301@users.noreply.github.com>
2018-09-04 12:02:53 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+print ("Hello, World!")
+
+#answer = input ("who inspires you?")
+#print(answer, "inspires you!")
+#myname = " Angela"
+
+#print ("My name is"  + myname)
+
+#if Age > 18:
+    #print ("You're an adult")
+#else:
+    #print ("You're underage")
+
+
+i= -1
+while True:
+    i+= 1
+
+    if(i>20):
+        break
+        # i is odd
+    if(i % 2 != 0):
+        continue
+
+    print(i)
